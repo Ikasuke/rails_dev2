e78eaead7d19d139a13458ef329f98bd3b5af7e7
井加田 洸輔 <ika_3_ika@yahoo.co.jp>
2018-10-14 17:38:35 +0900

    respecを試して消去した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_064554) do
+ActiveRecord::Schema.define(version: 2018_10_14_014832) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
